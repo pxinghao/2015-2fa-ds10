a947eed742e497478d186d4c4f10d41f8c1ffa56
Xinghao <pxinghao@gmail.com>
2015-08-01 12:18:33 -0700

    .

---
+++ project2/matlab/kNearestNeighbors.m
@@ -0,0 +1,23 @@
+function predictedClass = kNearestNeighbors(k, x, numClasses, class, counts, features)
+
+numData = length(class);
+
+% keep only relevant features
+counts = counts(:,features);
+x = x(:,features);
+
+% compute distances
+dist = sum(abs(repmat(x,[numData 1]) - counts),2);
+
+% pick top k
+topk = sortrows([dist (1:numData)']);
+full(topk(1:k,:))
+topk = topk(1:k,2);
+% full([-1 x; class(topk)' counts(topk,:)])
+
+votes = zeros([1 numClasses]);
+for i = 1:numClasses
+  votes(i) = sum(class(topk)==i);
+end
+
+[~, predictedClass] = max(votes);
